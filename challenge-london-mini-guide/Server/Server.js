a2492b160f5be4b3a88e5c5f57b51fe7a9cb9089
Nishka Kisten <nishka.crypto@gmail.com>
2022-12-02 19:41:53 +0200

    commit

--- challenge-london-mini-guide/Server/Server.js
+++ challenge-london-mini-guide/Server/Server.js
@@ -3,10 +3,11 @@ const app= express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-const Harrow = require('./data/Harrow.json');
-const Heathrow = require('./data/Heathrow.json');
-const Stratford =require('./data/Stratford.json');
-
+const harrow = require('./data/Harrow.json');
+const heathrow = require('./data/Heathrow.json');
+const stratford =require('./data/Stratford.json');
+// let cities = {Harrow: Harrow, Heathrow: Heathrow, Stratford:Stratford};
+// let {harrow, heathrow, stratford} = cities;
 app.get('/', (req, res) => {
     res.send(
         {
@@ -23,76 +24,86 @@ app.get('/', (req, res) => {
 // app.get('/pharmacies', (req, res) => {
 //     res.send(Stratford.pharmacies)
 // })
-app.get('/colleges', (req, res) => {
-    res.send(Stratford.colleges)
-})
+// app.get('/colleges', (req, res) => {
+//     res.send(Stratford.colleges)
+// })
 
-app.get('/doctors', (req, res) => {
-    res.send(Stratford.doctors)
-})
+// app.get('/doctors', (req, res) => {
+//     res.send(Stratford.doctors)
+// })
 
-app.get('/hospitals', (req, res) => {
-    res.send(Stratford.hospitals)
-})
+// app.get('/hospitals', (req, res) => {
+//     res.send(Stratford.hospitals)
+// })
 
-app.get('/:city/pharmacies', (req, res) => {
-let city = req.params.city;
-console.log(city);
-      if(city === "Harrow"){
-   res.send(Harrow.pharmacies)
-   }else if(city === "Heathrow"){
-      res.send(Heathrow.pharmacies) 
-    }else if(city === "Stratford"){
-       res.send(Stratford.pharmacies) 
-    }else {
-        res.send("Something went wrong")
-    }
-})
-
-app.get('/:city/:category', (req, res) => {
-    let city = req.params.city;
-    let category = req.params.category;
-        if(city === "Harrow"){
-            if(category === "Pharmacies"){
-                res.send(Harrow.pharmacies)
-            }else if(category === "Colleges"){
-                res.send(Harrow.colleges)
-            } else if(category === "Doctors") {
-                res.send(Harrow.doctors)
-            }else if(category === "Hospitals"){
-                res.send(Harrow.hospitals)
+    // app.get('/:city/pharmacies', (req, res) => {
+    // let city = req.params.city;
+    //     if(city === "Harrow"){
+    // res.send(Harrow.pharmacies)
+    // }else if(city === "Heathrow"){
+    //     res.send(Heathrow.pharmacies) 
+    //     }else if(city === "Stratford"){
+    //     res.send(Stratford.pharmacies) 
+    //     }else {
+    //         res.send("Something went wrong")
+    //     }
+    // })
+    // app.get("/:city/:category/search", (req,
+    //     res) => {
+    //     const city = req.params.city;
+    //     const category = req.params.category;
+    //     const term = req.query.term;
+    //     let data = cities[city][category].filter((category) =>
+    //       category["name"].toLowerCase().includes(term.toLowerCase())
+    //     );
+    //     res.status(200).json(data);
+    //   });
+    app.get('/:city/:category', (req, res) => {
+        let city = req.params.city;
+        let category = req.params.category;
+            if(city.toLowerCase() === "harrow"){
+                if(category === "Pharmacies" || category === "pharmacies"){
+                    res.send(harrow.pharmacies)
+                }else if(category === "Colleges" || category === "colleges" ){
+                    res.send(harrow.colleges)
+                } else if(category === "Doctors" || category === "doctors") {
+                    res.send(harrow.doctorsD)
+                }else if(category === "Hospitals" || category === "hospitals"){
+                    res.send(harrow.hospitals)
+                } else {
+                    res.send("Something went wrong")
+                }
+                // res.send(cities[city][category])
+        }else if(city === "Heathrow"){
+            if(category === "Pharmacies" || category === "pharmacies"){
+                res.send(heathrow.pharmacies)
+            }else if(category === "Colleges" || category === "colleges"){
+                res.send(heathrow.colleges)
+            } else if(category === "Doctors" || category === "doctors") {
+                res.send(heathrow.doctors)
+            }else if(category === "Hospitals" || category === "hospitals"){
+                res.send(heathrow.hospitals)
             } else {
                 res.send("Something went wrong")
             }
-       }else if(city === "Heathrow"){
-        if(category === "Pharmacies"){
-            res.send(Heathrow.pharmacies)
-        }else if(category === "Colleges"){
-            res.send(Heathrow.colleges)
-        } else if(category === "Doctors") {
-            res.send(Heathrow.doctors)
-        }else if(category === "Hospitals"){
-            res.send(Heathrow.hospitals)
-        } else {
-            res.send("Something went wrong")
-        }
-          
-        }else if(city === "Stratford"){
-            if(category === "Pharmacies"){
-                res.send(Stratford.pharmacies)
-            }else if(category === "Colleges"){
-                res.send(Stratford.colleges)
-            } else if(category === "Doctors") {
-                res.send(Stratford.doctors)
-            }else if(category === "Hospitals"){
-                res.send(Stratford.hospitals)
-            } else {
+            
+            }else if(city === "Stratford"){
+                if(category === "Pharmacies" || category === "pharmacies"){
+                    res.send(stratford.pharmacies)
+                }else if(category === "Colleges" || category === "colleges"){
+                    res.send(stratford.colleges)
+                } else if(category === "Doctors" || category === "doctors") {
+                    res.send(stratford.doctors)
+                }else if(category === "Hospitals" || category === "hospitals"){
+                    res.send(stratford.hospitals)
+                } else {
+                    res.send("Something went wrong")
+                }
+            }
+            else {
                 res.send("Something went wrong")
             }
-        }else {
-            res.send("Something went wrong")
-        }
-    })
+        })
     
     
 
